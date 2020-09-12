6a1561977cd2b08156b790d27f3a2facf4fe7803
Kenneth Huang <kennethhuang31@outlook.com>
2020-09-12 20:24:23 +1200

    new Home Page

---
+++ src/renderer/components/features/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Hello Kenneth
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data: function() {
+    return {
+      name: 'html'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
